071271f7ad662296172c64a761765f301c41dbe3
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-01 16:20:17 -0800

    engine is cleaned up but working

--- src/containers/Engine.jsx
+++ src/containers/Engine.jsx
@@ -1,42 +1,19 @@
 
 import React, { useState, useEffect, useRef } from 'react';
-// import styles from './engine.module.scss';
-// import useEvent from '../hooks/useEvent';
+
+import useEvent from '../hooks/useEvent';
 import Player from '../components/Player';
 import io from 'socket.io-client';
-// import Wall from '../components/Walls';
-// import Janitor from '../Janitor/Janitor';
-// import { objectArray } from '../../data/walls';
-// import handleKeyPress from '../utils/handleKeyPress';
-// import renderWalls from '../utils/renderWalls';
-// import renderUsers from '../utils/renderUsers';
+
 const serverUrl = process.env.REACT_APP_SERVER_URL;
 const socket = io.connect(serverUrl);
-
-
-
-// const bob = {
-//     position: { x: 50, y: 50 },
-//     dimension: { x: 50, y: 50 },
-//     storyBeat: 0,
-//     texts: [
-//         'Bob: HI Paul!',
-//         'Bob: Hi Sjaan!',
-//         'Bob: Hi Evan!'
-//     ]
-// };
-
-// const npcArray = [bob];
 
 export default function Engine() {
     const [userArray, setUserArray] = useState([]);
     const localUser = useRef(null);
-    // const [disable, setDisable] = useState(false);
-    // const [idleTimer, setIdleTimer] = useState();
 
     useEffect(() => {
         socket.on('CREATE_USER', ({ newUser, userArray }) => {
-            console.log(newUser);
             localUser.current = newUser;
             setUserArray(userArray);
         });
@@ -50,30 +27,22 @@ export default function Engine() {
     useEffect(() => {
         socket.emit('CREATE_USER', null);
 
-        // setInterval(() => {
-        //     socket.emit('GAME_STATE', localUser.current);
-        // }, 150);
+        setInterval(() => {
+            if (localUser.current) {
+                socket.emit('GAME_STATE', localUser.current);
+            }
+        }, 150);
     }, []);
 
 
-
-    // setIdleTimer(setTimeout(() => {
-    //     localUser.current = { ...localUser.current, dir: 'idle' };
-    // }, [400]));
-
-
-
-    // useEvent('keydown', handleKeyPress(e));
+    useEvent('keydown', localUser);
 
 
 
 
     return (
-        <div //className={styles.container}
-        // onClick={() => { engineFocused.current = true; }}
-        >
+        <div>
             <span />
-            {/* {renderUsers(userArray)} */}
             {localUser.current ?
                 <Player
                     key={localUser.current.id}
