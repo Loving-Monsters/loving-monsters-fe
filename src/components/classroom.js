0b2a705b03a33313bea9ef3ad08812fdd5184c8a
Evan Finkelstein <evanfinkelstein91@gmail.com>
2021-03-03 14:37:49 -0800

    room transitions

--- src/components/classroom.js
+++ src/components/classroom.js
@@ -1,16 +1,12 @@
-/* eslint-disable max-len */
-import { classroom } from './classroom';
-
-export const hallway = {
-    mapImage: '/hallway/MessyHallway.png',
+export const classroom = {
+    mapImage: '/classroom/Workroom.png',
     portals: {
         position: {
             x: 275,
             y: 400
-        },
-        nextMap: classroom,
+        }
     },
-
+    nextMap: '',
 
     objectArray: [
         {
@@ -69,34 +65,3 @@ export const hallway = {
         }
     ],
 };
-
-
-
-
-
-// const NorthWall = {
-//     position: { x: 0, y: 0 },
-//     dimension: { x: 1516, y: 400 }
-// };
-// const EastWall = {
-//     position: { x: 750, y: 0 },
-//     dimension: { x: 10, y: 640 }
-// };
-// const SouthWall = {
-//     position: { x: 0, y: 550 },
-//     dimension: { x: 1516, y: 10 }
-// };
-// const WestWall = {
-//     position: { x: -30, y: 0 },
-//     dimension: { x: 10, y: 640 }
-// };
-// const Lockers1 = {
-//     position: { x: 0, y: 325 },
-//     dimension: { x: 150, y: 100 }
-// };
-// const Lockers2 = {
-//     position: { x: 500, y: 325 },
-//     dimension: { x: 250, y: 100 }
-// };
-// export const hallArray = [NorthWall, EastWall, SouthWall, WestWall, Lockers1, Lockers2];
-// export const hallImage = '/hallway/MessyHallway.png'
