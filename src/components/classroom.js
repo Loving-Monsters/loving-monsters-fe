060d4bae580f26a56542de2584991ca47a9652ca
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-04 09:05:05 -0800

    update

--- src/components/classroom.js
+++ src/components/classroom.js
@@ -3,8 +3,8 @@ export const classroom = {
     mapImage: '/classroom/Workroom.png',
     portals: [{
         position: {
-            x: 275,
-            y: 400
+            x: 50,
+            y: 50
         },
         dimension: {
             x: 50,
