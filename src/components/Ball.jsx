1cc4f296a8cd6e9315cafcfa66b28679dcfb85c5
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-03-10 11:31:22 -0800

    ball resized

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -1,29 +1,21 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+/* eslint-disable max-len */
+import React from 'react';
 
 export default function Ball({ position, rotate }) {
     return (
         <div style={{
-            width: '50px',
-            height: '50px',
-            borderRadius: '100%',
+            width: '100px',
+            height: '100px',
             zIndex: '1',
             position: 'absolute',
-            border: 'solid black',
-            transform: `translate3d(${position.x}px, ${position.y}px, 0) rotate(${rotate}deg) `,
-            overflow: 'hidden',
-            backgroundImage: 'url(/snotball.png)',
-            backgroundSize: '100%',
-            backgroundPosition: 'center'
-
-            // transform: `rotate(${rotate}deg)`
-
+            transform: `translate3d(${position.x}px, ${position.y}px, 0) rotate(${rotate}deg) `
         }}>
-            {/* <img src="/snotball.png"
+            <img src="/snotball.png"
                 style={{
                     width: '100px',
                     height: '100px',
-
-                }} /> */}
+                }} />
         </div >
     );
 }
