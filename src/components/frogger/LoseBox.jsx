78f6597ba1204f9ff8215247264b95f4bd9f5df8
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-03-12 11:30:21 -0800

    winbox and losebox styled

--- src/components/frogger/LoseBox.jsx
+++ src/components/frogger/LoseBox.jsx
@@ -1,17 +1,15 @@
+/* eslint-disable react/prop-types */
+
 import React from 'react';
+import styles from './frogger.css';
 
 export default function LoseBox({ handleReset, handleEndGame }) {
     return (
         <>
-            <div style={{
-                zIndex: 1,
-                height: '200px',
-                width: '200px',
-                position: 'absolute',
-                border: 'solid black'
-            }}>
-                <div>You Died Horribly</div>
+            <div className={styles.gamebox}>
+                <p className={styles.lose}>You Died!</p>
                 <button onClick={() => handleReset()}>Try Again</button>
+                <br />
                 <button onClick={() => handleEndGame()}>Quit</button>
             </div>
         </>
--- src/components/frogger/WinBox.jsx
+++ src/components/frogger/WinBox.jsx
@@ -1,16 +1,16 @@
+/* eslint-disable react/prop-types */
+
 import React from 'react';
+import styles from './frogger.css';
 
 export default function WinBox({ handleReset, handleEndGame }) {
     return (
         <>
-            <div style={{
-                zIndex: 1,
-                height: '200px',
-                width: '200px',
-                position: 'absolute'
-            }}>
-                <div>Congrats You Win!  Everyone is very hoppy for you</div>
+            <div className={styles.gamebox}>
+                <p className={styles.win}>You Win!</p>
+                <p className={styles.subwin}>Everyone is very hoppy for you!</p>
                 <button onClick={() => handleReset()}> Play Again</button>
+                <br />
                 <button onClick={() => handleEndGame()}>Leave</button>
             </div>
         </>
