ee8b7db35a2da792bc682aa9b0495d13e4007e41
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-16 15:39:36 -0700

    removes redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
-import { Provider } from 'react-redux';
-import store from './store';
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById('root')
 );
